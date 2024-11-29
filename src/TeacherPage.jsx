fb6b94a91f7a2f42c2df179a49739b739c71b264
CodeSandbox <support@codesandbox.io>
2024-11-29 22:15:48 +0000

    Sandbox snapshot commit

--- src/TeacherPage.jsx
+++ src/TeacherPage.jsx
@@ -12,13 +12,11 @@ const TeacherPage = ({ children }) => {
         <thead className="bg-gray-800 text-white">
           <tr>
             <th className="w-1/3 px-4 py-2">Student</th>
-            <th className="w-1/3 px-4 py-2">Registration Status</th>
-            <th className="w-1/3 px-4 py-2">Week Indicator</th>
+            <th className="w-2/3 px-4 py-2">Week Indicators</th>
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-gray-100">
-          <td className="border px-4 py-2"></td>
+          {/* <tr className="bg-gray-100">
             <td className="border px-4 py-2">Name</td>
             <td className="border px-4 py-2">
               <div className="grid grid-cols-5 gap-2">
@@ -29,11 +27,14 @@ const TeacherPage = ({ children }) => {
                 <div>Fri</div>
               </div>
             </td>
-          </tr>
+          </tr> */}
           {children.map((child, index) => (
             <tr key={index} className="bg-gray-100">
-              <td className="border px-4 py-2">{ child.registration?.['mon']?.indicator ?? <GreyDot/>}</td> 
-              <td className="border px-4 py-2">{child.name}</td>
+              <td className="border px-4 py-2">
+                <div className="grid grid-cols-2 gap-2">
+                  <div>{child.name}</div>
+                  <div>{child.registration?.['mon']?.indicator ?? <GreyDot />}</div>
+                </div></td>
               <td className="border px-4 py-2">
                 <div className="grid grid-cols-5 gap-2">
                   <div>{child.registration['mon']?.indicator}</div>
